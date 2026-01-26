9805314116920b53457780c84dcab7584a2b1763
brianizquierdop <brian.izquierdop@gmail.com>
2026-01-26 18:35:45 -0500

    Remove unused Link import from InlineCartPage

--- src/InlineCartPage/InlineCartPage.js
+++ src/InlineCartPage/InlineCartPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import CollectJSSection from "./CollectJSSection";
-import {Link} from "react-router-dom";
+
 
 class InlineCartPage extends React.Component {
   constructor(props) {
